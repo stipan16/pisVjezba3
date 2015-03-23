a1d8f4e7d94cdd60d18546ff78cce24dc8c6c679
stipan16 <slausi00@fesb.hr>
2015-03-23 10:44:34 +0100

    prvi

---
+++ WalletProject23031993/Wallet.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WalletProject23031993
+{
+    public class Wallet
+    {
+        //konstruktor
+        public Wallet()
+        {
+            this.dictionary = new Dictionary<string, float>();
+        }
+
+        //metoda za dodavanje valuta
+        public void AddCurrencyValue(string currency, float value)
+        {
+            if (this.dictionary.ContainsKey(currency))
+            {
+                this.dictionary[currency] += value;
+            }
+            else
+            {
+                this.dictionary.Add(currency, value);
+            }
+        }
+
+        public float GetCurrencyValue(string currency)
+        {
+            //provjerava da li ima vrijednost tog ključa ---ako ga ima vrati vrijednost ,a ako ne vrati 0
+            return this.dictionary.ContainsKey(currency)
+                ? this.dictionary[currency]
+                : 0;
+        }
+
+
+        private Dictionary<string, float> dictionary;
+
+    }
+}
+
+//codeCoverageResults->Crveno što nie obrađeno,plavo ono što je izvršeno
+//Solution Items/Local.testsettings/2-click/Data&Diagnostics/CodeCoverage->Apply
+//CTRL+R   + A
---
+++ WalletProject23031993Test/WalletTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WalletProject23031993;
+
+namespace WalletProject23031993Test
+{
+    [TestClass]
+    public class WalletTest
+    {
+        //za runat test->Test/Run/All Tests in Solution
+    
+        [TestMethod]
+        public void TestEmptyWalletReturnsZero()
+        {
+            var wallet = new Wallet();
+
+            Assert.AreEqual(
+                0,
+                wallet.GetCurrencyValue("HRK"),
+                "empty wallet should return Zero"
+                // što nam treba vratit; s čim se uspoređuje; poruka u slučaju rezultata različita od 0.
+
+            );
+        }
+
+        [TestMethod]
+        public void TestSingleIOtemWalletGetCurrency()
+        {
+            var wallet = new Wallet();
+
+            wallet.AddCurrencyValue("HRK", 100);
+            
+            Assert.AreEqual(
+                100,
+                wallet.GetCurrencyValue("HRK"),
+                "single item wallet bla bla"
+                );
+        }
+
+        //svaka metoda koju zelimo testirat mora ima prije [TestMethod]
+        [TestMethod]
+        public void TestMultipleSameCurrencyWalletGetCurrency()
+        {
+            var wallet = new Wallet();
+
+            wallet.AddCurrencyValue("HRK", 100);
+            wallet.AddCurrencyValue("HRK", 50);
+
+            Assert.AreEqual(
+                150,
+                wallet.GetCurrencyValue("HRK"),
+                "multiple item wallet bla bla"
+                );
+        }
+
+        [TestMethod]
+        public void TestAddCurrencyCrashesIfFormatLengthInvalid()
+        {
+
+        }
+
+        }
+}
